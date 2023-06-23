295301b2e3a6a7e0fbcd4d045acc17310274d5c9
brorica <minjea917@gmail.com>
2023-06-23 18:37:19 +0900

    red black tree 삭제 일부 구현

--- src/RedBlackTree.java
+++ src/RedBlackTree.java
@@ -60,7 +60,7 @@ public class RedBlackTree {
         newNode.right = nilNode;
         // 새로운 노드를 삽입할 위치를 찾음
         Node x = root;
-        Node y = null;
+        Node y = nilNode;
         while (x != nilNode) {
             y = x;
             if (x.data <= data) {
@@ -71,7 +71,7 @@ public class RedBlackTree {
         }
         // 새로운 노드가 삽입될 부모 노드를 찾음
         newNode.parent = y;
-        if (y == null) {
+        if (y == nilNode) {
             root = newNode;
             root.color = 'B';
             return;
@@ -185,5 +185,84 @@ public class RedBlackTree {
             }
             System.out.println();
         }
+    }
+
+    private static void delete(Integer data) {
+        Node delNode = nilNode;
+        Node x = root;
+        while (x != nilNode) {
+            if (x.data == data) {
+                delNode = x;
+            }
+            if (x.data <= data) {
+                x = x.right;
+            } else {
+                x = x.left;
+            }
+        }
+        if (delNode == nilNode) {
+            return;
+        }
+        int delNodeColor = delNode.color;
+        Node swapNode;
+        // 자식이 하나만 있을 때 -> 오른쪽 자식만 가지는 경우
+        if (delNode.left == nilNode) {
+            swapNode = delNode.right;
+            transPlant(delNode, delNode.right);
+        }
+        // 자식이 하나만 있을 때 -> 왼쪽 자식만 가지는 경우
+        else if (delNode.right == nilNode) {
+            swapNode = delNode.left;
+            transPlant(delNode, delNode.left);
+        }
+        // 자식이 둘 다 있거나 없을 떄
+        else {
+            swapNode = findSuccessor(delNode.right);
+            delNodeColor = swapNode.color;
+            // Successor 과정에서 반환되는 노드는 오른쪽 자식 노드 or 자식이 없음
+            Node swapNodeChild = swapNode.right;
+            // 스왑 노드의 부모가 삭제할 노드이고, 자식 노드가 nil Node면 부모 값 넣어야 함
+            if (swapNode.parent == delNode) {
+                swapNodeChild.parent = swapNode;
+            } else {
+                // swapNode는 삭제할 노드로 이동하기 때문에 자식 노드를
+                // swapNode의 부모 밑에 붙여야 함
+                transPlant(swapNode, swapNode.right);
+                // delNode의 오른쪽 서브트리는 Successor보다 크기 때문에 오른쪽에 붙임
+                swapNode.right = delNode.right;
+                swapNode.right.parent = swapNode;
+            }
+            // delNode의 부모가 swapNode를 참조하게 함
+            transPlant(delNode, swapNode);
+            // delNode의 왼쪽 서브트리는 Successor보다 작기 때문에 오른쪽에 붙임
+            swapNode.left = delNode.left;
+            // delNode의 왼쪽 서브트리가 swapNode를 참조하게 함
+            // 이제 delNode를 참조하는 객체가 없으므로 delNode는 gc의 대상이 됨
+            swapNode.left.parent = swapNode;
+            // swapNode가 delNode의 자리로 간 것이니, delNode의 색 부여
+            swapNode.color = delNode.color;
+        }
+        // 삭제된 노드가 블랙 노드면 red black 트리에 불균형 발생
+        if (delNodeColor == 'B') {
+            // 복구작업
+        }
+    }
+
+    private static void transPlant(Node node, Node child) {
+        if (node.parent == nilNode) {
+            root = child;
+        } else if (node.parent.left == node) {
+            node.parent.left = child;
+        } else {
+            node.parent.right = child;
+        }
+        child.parent = node.parent;
+    }
+
+    private static Node findSuccessor(Node node) {
+        if (node.left == nilNode) {
+            return node;
+        }
+        return findSuccessor(node.left);
     }
 }
