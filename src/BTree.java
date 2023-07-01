f6da99fa5ca465fa95f3953ff5fc76ecf6f20185
brorica <minjea917@gmail.com>
2023-07-01 17:52:51 +0900

    b tree 삽입 구현

--- src/BTree.java
+++ src/BTree.java
@@ -7,75 +7,130 @@ public class BTree {
         boolean leaf;
 
         public Node() {
-            child = new Node[MAX_NODE_INDEX];
-            key = new Integer[MAX_KEY_INDEX];
+            child = new Node[MAX_DEGREE + 1];
+            key = new Integer[MAX_DEGREE];
             n = 0;
             leaf = true;
         }
     }
 
-    private static final int MAX_DEGREE = 3;
-    private static final int MAX_NODE_INDEX = 2 * MAX_DEGREE;
-    private static final int MAX_KEY_INDEX = 2 * MAX_DEGREE - 1;
+    private static final int DEGREE = 2;
+    private static final int MAX_DEGREE = 2 * DEGREE;
     private static Node root = new Node();
 
     public static void main(String[] args) {
-
-    }
-
-    private static Node find(Node node, Integer value) {
-        int index = 0;
-        // 이분 탐색으로 찾는 방법이 있지만, 코드 간소화를 위해 순차 탐색을 이용
-        while (node.n > index && node.key[index] < value) {
-            index += 1;
-        }
-        // 값을 찾은 경우
-        if (index < node.n && node.key[index].equals(value)) {
-            return node;
-        }
-        // 여기까지 왔단 것은, 리프 노드에도 해당 value를 가지는 노드가 없단 뜻
-        if (node.leaf) {
-            return null;
-        }
-        // index < node.n 일때 node.key[index] < data < node.key[index + 1]
-        // index >= node.n 일때 data >= node.key[index + 1]
-        return find(node.child[index], value);
+        insert(18);
+        insert(4);
+        insert(9);
+        insert(13);
+        insert(1);
+        insert(11);
+        insert(5);
+        insert(20);
+        insert(7);
+        insert(16);
+        insert(10);
+        insert(2);
+        insert(8);
+        insert(19);
+        insert(6);
+        insert(3);
+        insert(15);
+        insert(12);
+        insert(17);
+        insert(14);
+        print(root);
     }
 
     private static void insert(Integer value) {
         Node node = root;
-        if (node.n == 2 * MAX_DEGREE - 1) {
+        addKey(node, value);
+        if (node.n == MAX_DEGREE) {
             Node newRoot = new Node();
             newRoot.leaf = false;
             newRoot.child[0] = root;
             root = newRoot;
-        }
-        if (node.n < 2 * MAX_DEGREE - 1) {
-
+            split(newRoot, 0, value);
         }
     }
 
-    private static void split(Node node, int index) {
+    // split 기준은 2가지로 나뉜다.
+    // 노드를 추가한 후 split
+    // split 수행 후 노드 추가
+    // 여기선 노드를 추가한 후 split 수행
+    private static void split(Node node, int index, Integer value) {
         Node rightNode = new Node();
-        Node leftNode = node.child[0];
+        Node leftNode = node.child[index];
+        if (node.key[index] != null) {
+            int temp = node.n;
+            for (int i = node.n - 1; i >= 0 && node.key[i] > value; i--) {
+                node.key[i + 1] = node.key[i];
+                node.child[i + 2] = node.child[i + 1];
+                temp -= 1;
+            }
+            leftNode = node.child[temp];
+        }
         // rightNode는 leftNode와 같은 레벨이기 때문에, leaf 값이 같다.
         rightNode.leaf = leftNode.leaf;
-        // MAX_DEGREE - 3 위치에 있는 키 값은 위로 올라간다.
-        int splitIndex = MAX_DEGREE - 2;
-        rightNode.n = splitIndex;
+        // 오른쪽 노드를 오른쪽부터 DEGREE 수 만큼 분리된다.
+        rightNode.n = DEGREE;
         // key 배열 정리
-        for (int i = 0; i < splitIndex; i++) {
-            rightNode.key[i] = leftNode.key[splitIndex + i];
-            leftNode.key[splitIndex + i] = 0;
+        for (int i = 0; i < DEGREE; i++) {
+            rightNode.key[i] = leftNode.key[i + DEGREE];
+            leftNode.key[i + DEGREE] = null;
             leftNode.n -= 1;
         }
-        // child 배열 정리
-        int splitChildIndex = MAX_DEGREE - 1;
-        for (int i = 0; i < splitChildIndex; i++) {
-            rightNode.child[i] = leftNode.child[i + splitChildIndex];
-            leftNode.child[i + splitChildIndex] = null;
+        // child 배열 정리, leaf 노드면 수행하는 의미가 없다.
+        // leaf 유무에 대해 분기를 타야하지만 단순화를 위해 적용하진 않음
+        // 자식 노드는 키보다 1 더 많기 때문에 i <= DEGREE 했다.
+        for (int i = 0; i <= DEGREE; i++) {
+            rightNode.child[i] = leftNode.child[i + DEGREE];
+            leftNode.child[i + DEGREE] = null;
         }
-        // 노즈의 자식 노드 마지막에 rightNode 추가
+        // leftNode의 마지막 키를 위로 올림
+        node.key[index] = leftNode.key[DEGREE - 1];
+        node.n += 1;
+        leftNode.key[DEGREE - 1] = null;
+        leftNode.n -= 1;
+        // 노드의 자식 노드 마지막에 rightNode 추가
         node.child[index + 1] = rightNode;
+
     }
+
+    private static void addKey(Node node, Integer value) {
+        int index;
+        if (node.leaf) {
+            index = node.n;
+            for (int i = node.n - 1; i >= 0 && node.key[i] > value; i--) {
+                node.key[i + 1] = node.key[i];
+                index -= 1;
+            }
+            node.key[index] = value;
+            node.n += 1;
+        }
+        else {
+            index = 0;
+            while (node.n > index && node.key[index] < value) {
+                index += 1;
+            }
+            Node there = node.child[index];
+            addKey(there, value);
+            if (there.n == MAX_DEGREE) {
+                split(node, index, value);
+            }
+        }
+    }
+
+    private static void print(Node node) {
+        int N = node.n;
+        for (int i = 0; i < N; i++) {
+            System.out.print(node.key[i] + " ");
+        }
+        if (!node.leaf) {
+            for (int i = 0; i < N + 1; i++) {
+                print(node.child[i]);
+            }
+        }
+    }
+
 }
